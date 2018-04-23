76211245cffcff32631e027c10144828e97ca529
Peter Naimoli <pnaimoli@users.noreply.github.com>
2018-04-23 13:43:02 -0500

    Add a README.md and make things easier to run

---
+++ README.md
@@ -0,0 +1,21 @@
+# Project Title
+
+Double Dummy Analyzer - a crummy double dummy solver with some cool features!
+
+## Getting Started
+
+### Prerequisites
+
+```
+[Boost](https://github.com/boostorg/boost) - to compile libdda.so
+Python3 - to run ehas.py
+```
+
+### Building
+
+make -C src
+cd redeal/dds/src && make -f Makefiles/Makefile_Mac_clang_shared && cp libdds.so ../../
+
+### Running
+
+python3 -u -m redeal.redeal ehas.py | tee output.pbn
--- ehas.py
+++ ehas.py
@@ -1,9 +1,18 @@
-from redeal import *
+from redeal.redeal import *
 import subprocess
 import src.libdda as libdda
 
 predeal = {"S": SmartStack(balanced + semibalanced, hcp, range(12,18)),
           }
+
+FOUND_SQUEEZE = 0
+FAILED_SQUEEZE = 0
+
+def initial(deal):
+    # Output in pbn format
+    redeal.Hand.set_str_style("pbn")
+    redeal.Deal.set_str_style("pbn")
+    print("[Event \"EHAS!\"]")
 
 def accept(deal):
     if deal.dd_tricks("3NS") != 9:
@@ -20,7 +29,17 @@ def accept(deal):
     dda.give_pitch(2)
 
     if dda.analyze(4) != 4:
+        global FOUND_SQUEEZE
+        print()
+        print("[Board \"{}\"]".format(FOUND_SQUEEZE + 1))
+        print("[Dealer \"S\"]")
+        print("[Vulnerable \"None\"]")
+        FOUND_SQUEEZE += 1
         return True
     else:
-        print("Failed")
+        global FAILED_SQUEEZE
+        FAILED_SQUEEZE += 1
         return False
+
+def final(n_tries):
+    print("% Found {} Tried {}".format(FOUND_SQUEEZE, FAILED_SQUEEZE))
